3450f201d9d342590afe2f888d637e2914b42523
Caro-vcg <42705387+Caro-vcg@users.noreply.github.com>
2019-02-14 11:54:19 -0600

    Update Challenge.mosaico.js

--- Challenge.mosaico.js
+++ Challenge.mosaico.js
@@ -6,7 +6,7 @@ function draw() {
   background(220);
   for (var y = 5; y < 500; y = y + 10) {
     for (var x = 5; x < 500; x = x + 10) {
-      fill(80, 150, 200);
+      fill(random(0,255), random(0,255), 200);
       ellipse(x, y, 10, 10);
     }
   }
